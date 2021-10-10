6d1b94abc9d7e988161cfbd65911884572c80f2d
Rafael Alberto <rafael.alberto1703@gmail.com>
2021-10-10 17:46:31 -0300

    add adapter

--- src/infra/repository/ParkingLotRepositoryMemory.ts
+++ src/infra/repository/ParkingLotRepositoryMemory.ts
@@ -1,19 +1,25 @@
+import ParkingLotAdapter from '../../adapter/ParkingLotAdapter'
 import ParkedCar from '../../core/entity/ParkedCar'
 import ParkingLot from '../../core/entity/ParkingLot'
 import ParkingLotRepository from '../../core/repository/ParkingLotRepository'
 
-export default class ParkingLotRepositoryMemory
-implements ParkingLotRepository {
-  parkingLots: Array<ParkingLot> = [new ParkingLot('mall', 5, 8, 18, 0)]
+export default class ParkingLotRepositoryMemory implements ParkingLotRepository {
+  parkingLots = [
+    {
+      code: 'mall',
+      capacity: 5,
+      open_hour: 8,
+      close_hour: 18
+    }
+  ]
 
   parkedCars: Array<ParkedCar> = []
 
   getParkintLot (code: string): Promise<ParkingLot> {
-    const parkingLot = this.parkingLots.find((item) => item.code === code)
-    if (parkingLot) {
-      parkingLot.occupiedSpaces = this.parkedCars.length
-    }
-    return Promise.resolve(parkingLot!)
+    const parkingLotData = this.parkingLots.find((item) => item.code === code)
+    const occupiedSpaces = this.parkedCars.length
+    const parkingLot = ParkingLotAdapter.create(parkingLotData.code, parkingLotData.capacity, parkingLotData.open_hour, parkingLotData.close_hour, occupiedSpaces)
+    return Promise.resolve(parkingLot)
   }
 
   parkCar (code: string, plate: string, date: Date): void {
--- test/EnterParkingLotUseCase.test.ts
+++ test/EnterParkingLotUseCase.test.ts
@@ -14,11 +14,7 @@ test('Should enter parking lot', async function () {
   const parkingLotBeforeEnter = await getParkingLotUseCase.execute('mall')
   expect(parkingLotBeforeEnter.occupiedSpaces).toBe(0)
 
-  await enterParkingUseCase.execute(
-    'mall',
-    'ABC-1234',
-    new Date('2021-10-09 09:00:00')
-  )
+  await enterParkingUseCase.execute('mall', 'ABC-1234', new Date('2021-10-09 09:00:00'))
 
   const parkingLotAfterEnter = await getParkingLotUseCase.execute('mall')
   expect(parkingLotAfterEnter.occupiedSpaces).toBe(1)
